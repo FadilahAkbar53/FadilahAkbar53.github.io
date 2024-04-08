f222a234809af4435324ef127aac95f0dac63582
FadilahAkbar53 <fadilahakbar310504@gmail.com>
2024-04-08 09:44:47 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
- const mntoggle = document.querySelector('.menu-toggle input');
- const nav = document.querySelector('nav ul');
+const mntoggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
 
-mntoggle.addEventListener('click',function(){
-    nav.classList.toggle('menushow');
-})
+mntoggle.addEventListener("click", function () {
+  nav.classList.toggle("menushow");
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides-fade");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
